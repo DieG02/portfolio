7ecdb847cec23b55870d005e177b41de902c3443
DieG02 <43993554@alejandrovolta.edu.ar>
2021-10-05 14:48:25 -0300

    navBar styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
-import './styles/main.scss';
+import './styles/App.scss';
 
 function App() {
   return (
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import github from '../assets/github.svg';
+import linkedIn from '../assets/linkedIn.svg';
 import '../styles/NavBar.scss';
 
 export default function NavBar () {
   return (
-    <div>
-      <p>Navbar</p>
+    <div className='navbar-container'>
+      <div>
+        <p>Nombre</p>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href='#'>Inicio</a>
+          </li>
+          <li>
+            <a href='#'>Sobre mi</a>
+          </li>
+          <li>
+            <a href='#'>Skills</a>
+          </li>
+          <li>
+            <a href='#'>Proyectos</a>
+          </li>
+          <li>
+            <a href='#'>Contacto</a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <a href='#'>
+          <image alt='logo_' src={github} width='30px'/>
+        </a>
+        <a href='#'>
+          <image alt='logo_' src={linkedIn} width='30px'/>
+        </a>
+      </div>
     </div>
   )
 }
