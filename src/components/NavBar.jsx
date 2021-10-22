98ffe8b5df2a1faa54bfeff86285628f6c1dcdc0
DieG02 <43993554@alejandrovolta.edu.ar>
2021-10-22 18:25:46 -0300

    logo fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logo.svg';
+import Logo from '../assets/Mask_icon.svg';
 import '../styles/NavBar.scss';
 
 export default function NavBar () {
